37be1393e76b601d5683535169181aa1945b0fa0
Flora <fmacuacua116@gmail.com>
2026-03-15 13:01:35 -0700

    actualizei

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let menu = document.getElementById("menu");
 menuBtn.onclick = function(){
 menu.classList.toggle("active");
 }
+
+const img_dialog=document.querySelector('img.img_dialog')
+const dialog=document.querySelector('dialog')
+const fechar=document.querySelector('span.fechar_dialog')
+
+img_dialog.onclick=function(){
+    dialog.showModal()
+}
+fechar.onclick=function(){
+    dialog.close()
+}
+
+dialog.addEventListener("click",e=>{
+    if(e.target===dialog){
+        dialog.close()
+    }
+  }
+
+)
