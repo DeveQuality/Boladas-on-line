a94bc06c77063d741edf46b4c6cf8eb6c4a3a5e7
Flora <fmacuacua116@gmail.com>
2026-03-29 12:43:48 -0700

    tirei o scroll do dialog

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ document.querySelectorAll(".img img").forEach(img => {
     img.addEventListener("click", () => {
         const dialog = img.parentElement.querySelector("dialog");
         dialog.showModal();
+
+        // BLOQUEAR SCROLL
+        document.body.classList.add("no-scroll");
     });
 });
 
 // FECHAR DIALOG
 document.querySelectorAll(".fechar_dialog").forEach(btn => {
     btn.addEventListener("click", () => {
-        btn.closest("dialog").close();
+        const dialog = btn.closest("dialog");
+        dialog.close();
+
+        // LIBERAR SCROLL
+        document.body.classList.remove("no-scroll");
     });
 });
 
@@ -32,6 +39,9 @@ document.querySelectorAll("dialog").forEach(dialog => {
     dialog.addEventListener("click", e => {
         if (e.target === dialog) {
             dialog.close();
+
+            // LIBERAR SCROLL
+            document.body.classList.remove("no-scroll");
         }
     });
 });
